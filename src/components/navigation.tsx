22230352273ebd33e5e47f8ad3868f2709490857
Lenzi Gall <lenzi.erickson@gmail.com>
2022-06-18 17:34:34 -0700

    remove nav bullets

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -14,11 +14,6 @@ export const Navigation: FC = () => {
   const xsBreakpoint = useMediaQuery(theme.breakpoints.only("xs"));
   const smBreakpoint = useMediaQuery(theme.breakpoints.only("sm"));
 
-  // TODO: figure out a better way to add the bullet dividers
-  // if it wraps there is always a bullet on the top we don't want
-  //  until then just do it on lg and up
-  const lgBreakpointAndUp = useMediaQuery(theme.breakpoints.up("lg"));
-
   return (
     <AppBar
       position="static"
@@ -33,12 +28,16 @@ export const Navigation: FC = () => {
           justifyContent: "center",
           py: 1,
           px: xsBreakpoint ? 4 : smBreakpoint ? 7 : 10,
-          columnGap:
-            xsBreakpoint || lgBreakpointAndUp ? 4 : smBreakpoint ? 7 : 10,
+          columnGap: xsBreakpoint ? 4 : smBreakpoint ? 7 : 10,
           rowGap: 1,
           flexWrap: "wrap",
         }}
       >
+        {/*
+          TODO: figure out a good way to add bullets in between links
+          but not have a hanging bullet when links wrap
+          bullets would be the theme secondary color
+        */}
         <Typography variant="h6" color="inherit">
           <Link
             underline="hover"
@@ -49,17 +48,6 @@ export const Navigation: FC = () => {
             Home
           </Link>
         </Typography>
-        {lgBreakpointAndUp && (
-          <Box
-            component="span"
-            sx={{
-              px: xsBreakpoint ? 1 : smBreakpoint ? 2.5 : 4,
-              color: "secondary.main",
-            }}
-          >
-            •
-          </Box>
-        )}
         <Typography variant="h6" color="inherit">
           <Link
             color="inherit"
@@ -71,17 +59,6 @@ export const Navigation: FC = () => {
             Events
           </Link>
         </Typography>
-        {lgBreakpointAndUp && (
-          <Box
-            component="span"
-            sx={{
-              px: xsBreakpoint ? 1 : smBreakpoint ? 2.5 : 4,
-              color: "secondary.main",
-            }}
-          >
-            •
-          </Box>
-        )}
         <Typography variant="h6" color="inherit">
           <Link
             color="inherit"
@@ -93,17 +70,6 @@ export const Navigation: FC = () => {
             Join
           </Link>
         </Typography>
-        {lgBreakpointAndUp && (
-          <Box
-            component="span"
-            sx={{
-              px: xsBreakpoint ? 1 : smBreakpoint ? 2.5 : 4,
-              color: "secondary.main",
-            }}
-          >
-            •
-          </Box>
-        )}
         <Typography variant="h6" color="inherit">
           <Link
             color="inherit"
