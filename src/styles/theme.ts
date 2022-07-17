e46b5c29082b526cf3e98f154c0142c38ca64e81
Lenzi Erickson <lenzi.erickson@gmail.com>
2022-07-16 19:56:33 -0700

    font size tweak

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -21,8 +21,8 @@ const theme = createTheme({
   typography: {
     fontFamily: `"Fira Code", monospace`,
     h1: {
-      letterSpacing: "0.575rem",
-      fontSize: "5.75rem",
+      letterSpacing: "0.6rem",
+      fontSize: "6rem",
       textTransform: "uppercase",
       fontWeight: 400,
       fontFamily: "Oswald, Arial, Helvetica, sans-serif",
@@ -35,8 +35,8 @@ const theme = createTheme({
       fontFamily: "Oswald, Arial, Helvetica, sans-serif",
     },
     h3: {
-      letterSpacing: "0.425rem",
-      fontSize: "4.25rem",
+      letterSpacing: "0.4rem",
+      fontSize: "4rem",
       textTransform: "uppercase",
       fontWeight: 400,
       fontFamily: "Oswald, Arial, Helvetica, sans-serif",
