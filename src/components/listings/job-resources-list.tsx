9018b01df2cc380bdf5f4ee751825e5f7e42fff8
Lenzi Erickson <lenzi.erickson@gmail.com>
2022-07-16 22:35:47 -0700

    no linkedInJobsUrls on JobResourcesType

--- src/components/listings/job-resources-list.tsx
+++ src/components/listings/job-resources-list.tsx
@@ -1,6 +1,5 @@
-import { IconButton, Grid, Typography, lighten } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { FC } from "react";
-import Image from "next/image";
 import { jobResources } from "../../data/job-resources";
 import { Link } from "../link";
 
@@ -15,12 +14,8 @@ export const JobResourcesList: FC = () => {
       {Object.keys(jobResources)
         .sort()
         .map((key) => {
-          const {
-            displayName,
-            websiteUrl,
-            websiteDisplayName,
-            linkedInJobsUrl,
-          } = jobResources[key];
+          const { displayName, websiteUrl, websiteDisplayName } =
+            jobResources[key];
           return (
             <Grid item key={key} xs={12} lg={6} xl={4} data-testid={key}>
               <Typography variant="body1" component="h5">
@@ -40,43 +35,6 @@ export const JobResourcesList: FC = () => {
                       .split("?")[0]}
                 </Link>
               </Typography>
-              {/* get better icons and use svgs and SvgIcon from MUI */}
-              {linkedInJobsUrl && (
-                <Link
-                  underline="hover"
-                  href={linkedInJobsUrl}
-                  target="_blank"
-                  sx={{
-                    display: "block",
-                    "&:hover": {
-                      "& .MuiIconButton-root": {
-                        background: lighten("#0e50b4", 0.25),
-                      },
-                    },
-                  }}
-                >
-                  <IconButton
-                    sx={{
-                      background: "#0e50b4",
-                      mt: 0.5,
-                    }}
-                  >
-                    <Image
-                      alt="LinkedIn"
-                      src="/images/linkedin.png"
-                      width={12}
-                      height={12}
-                    />
-                  </IconButton>
-                  <Typography
-                    variant="body2"
-                    component="p"
-                    sx={{ display: "inline", marginLeft: 0.5 }}
-                  >
-                    LinkedIn Jobs
-                  </Typography>
-                </Link>
-              )}
             </Grid>
           );
         })}
