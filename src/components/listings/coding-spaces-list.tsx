9d999975ecb59ec5cdf4480b0a933306c80ab66c
Lenzi Erickson <lenzi.erickson@gmail.com>
2025-11-08 12:05:23 -0800

    fix sort

--- src/components/listings/coding-spaces-list.tsx
+++ src/components/listings/coding-spaces-list.tsx
@@ -22,7 +22,7 @@ export const CodingSpacesList: FC = () => {
       <Box sx={scrollDivStyles}>
         <Grid container spacing={3} pb={2}>
           {Object.entries(codingSpaces)
-            .sort((a, b) => (a[1].displayName > b[1].displayName ? -1 : 1))
+            .sort((a, b) => b[1].displayName.localeCompare(a[1].displayName))
             .map(([key, value]) => {
               const { displayName, websiteUrl, cityNames } = value;
 
